7f73ea4be9560dc34d76239d014b9398edddebb9
Dmitry <dimakameya@gmail.com>
2023-04-17 19:39:52 +0500

    DZ1

--- DZ/DZ1/Homework_4.java
+++ DZ/DZ1/Homework_4.java
@@ -10,46 +10,44 @@ import java.util.Scanner;
 
 public class Homework_4 {
     public static void main(String[] args) {
-        char a = 1;
-        char b = 63;
-        char c = 63;
-        char d = 5;
-        char e = 6;
-        char f = 9;
+        int a = 1;
+        int b = 63;
+        int c = 63;
+        int d = 5;
+        int e = 6;
+        int f = 9;
+        int res = 0;
 
-        if (a == 63) {
-            for (int i = 0; i < 10; i++) {
-                a = (char) i;
-                if (b == 63) {
-                    for (int j = 0; j < 10; j++) {
-                        b = (char) j;
-                        if (c == 63) {
-                            for (int l = 0; l < 10; l++) {
-                                c = (char) l;
-                                if (d == 63) {
-                                    for (int k = 0; k < 10; k++) {
-                                        d = (char) k;
-                                        if (e == 63) {
-                                            for (int n = 0; n < 10; n++) {
-                                                e = (char) n;
-                                                if (f == 63) {
-                                                    for (int m = 0; m < 10; m++) {
-                                                        f = (char) m;
-                                                        if (a * 10 + b + c * 10 + d == e * 10 + f) {
-                                                            System.out.printf("%d%d + %d%d = %d%d", a, b, c, d, e, f);
-                                                            
-                                                        }
-                                                    }
-                                                }
-                                            }
-                                        }
-                                    }
-                                }
-                            }
+        
+        for (int i = 0; i < 10; i++) {
+            if (a == 63) a = i;
+            
+            for (int j = 0; j < 10; j++) {
+                if (b == 63) b = j;
+                    
+                for (int l = 0; l < 10; l++) {
+                    if (c == 63) c = l;
+                            
+                    for (int k = 0; k < 10; k++) {
+                        if (d == 63) d = k;
+                                    
+                        // for (int n = 0; n < 10; n++) {
+                        //     if (e == 63) e = n;
+                                            
+                        //     for (int m = 0; m < 10; m++) {
+                        //         if (f == 63) f = m;
+                                res = a * 10 + b + c * 10 + d;
+                                
+                                if (a * 10 + b + c * 10 + d == e * 10 + f) {
+                                    System.out.printf("%d%d + %d%d = %d%d", a, b, c, d, e, f);
+                            //     }
+
+                            // }
                         }
                     }
                 }
             }
+            System.out.println(res);
         }
     }
 }
