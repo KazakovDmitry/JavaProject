780b82cff24d1927dba4737144bd89d3167b6448
Dmitry <dimakameya@gmail.com>
2023-05-14 20:06:30 +0500

    horse

--- Seminars/sem5/dop6_koni.java
+++ Seminars/sem5/dop6_koni.java
@@ -17,10 +17,11 @@ public class dop6_koni {
         }
         System.out.println();
 
-        int x = 3; // координата по горизонтали
+        int x = 0; // координата по горизонтали
         int y = 0; // координата по вертикали
         int move = 1; //номер шага
         int start = 1;
+        int maxMove = 1;
         
         
         String[] coordinats = new String[3];
@@ -31,6 +32,11 @@ public class dop6_koni {
         System.out.println(journalMove);
 
         while (move < n*n) {
+            if(move == 0) {
+                System.out.printf("С данной стартовой точки решение не найдено. Максимальное количество ходов = ", maxMove);
+                System.out.println();
+                break;
+            }
             int i = start;
             while(i <= 8) {  // 8 - варианты хода конём
                 if(horse_move(x, y, arr, move, journalMove, i).containsKey(move+1) != false) {
@@ -39,26 +45,47 @@ public class dop6_koni {
                     System.out.println(journalMove);
                     break;
                 }
+                i++;
             }
             if(journalMove.containsKey(move+1)){
                 move++;
+                if(maxMove < move) maxMove = move;
                 coordinats = journalMove.get(move).split(",");
                 x = Integer.parseInt(coordinats[0]);
                 y = Integer.parseInt(coordinats[1]);
+                arr[x][y] = move;
+                start = 1;
             }
             else {
                 arr[x][y] = 0;
-                move--;
-                coordinats = journalMove.get(move).split(",");
+                journalMove.remove(move);
+                coordinats = journalMove.get(--move).split(",");                
+                System.out.println(journalMove);
+                System.out.println(Arrays.toString(coordinats));
                 x = Integer.parseInt(coordinats[0]);
                 y = Integer.parseInt(coordinats[1]);
-                start = Integer.parseInt(coordinats[2]);
+                start = Integer.parseInt(coordinats[2]) + 1;
+                System.out.printf("start=%d", start);
+                System.out.println();
+
+                while (start == 9) {
+                    arr[x][y] = 0;
+                    journalMove.remove(move);
+                    coordinats = journalMove.get(--move).split(",");  
+                    x = Integer.parseInt(coordinats[0]);
+                    y = Integer.parseInt(coordinats[1]);
+                    start = Integer.parseInt(coordinats[2]) + 1;
+                }
             }
 
         }
         
-        for (int i = 0; i < n; i++) {
-            System.out.println(Arrays.toString(arr[i]));
+        for (int[] is : arr) {
+            System.out.print("[");
+            for (int i : is) {
+                System.out.printf("%3d ", i);               
+            }
+            System.out.println("]");
         }
     }
 
@@ -78,51 +105,38 @@ public class dop6_koni {
                 break;
 
             case 4:
-                if (x + 1 < len && y - 2 >= 0  && arr[x + 1][y - 2] == 0) { x = x + 1; y = y - 2; arr[x][y] = 0;}
+                if (x + 1 < len && y - 2 >= 0  && arr[x + 1][y - 2] == 0) { x = x + 1; y = y - 2; arr[x][y] = ++move;}
                 break;
 
             case 5:
-                if (x - 2 >= 0  && y - 1 >= 0  && arr[x - 2][y - 1] == 0) { x = x - 2; y = y - 1; arr[x][y] = 0;}
+                if (x - 2 >= 0  && y - 1 >= 0  && arr[x - 2][y - 1] == 0) { x = x - 2; y = y - 1; arr[x][y] = ++move;}
                 break;
 
             case 6:
-                if (x - 2 >= 0  && y + 1 < len && arr[x - 2][y + 1] == 0) { x = x - 2; y = y + 1; arr[x][y] = 0;}
+                if (x - 2 >= 0  && y + 1 < len && arr[x - 2][y + 1] == 0) { x = x - 2; y = y + 1; arr[x][y] = ++move;}
                 break;
 
             case 7:
-                if (x - 1 >= 0  && y - 2 >= 0  && arr[x - 1][y - 2] == 0) { x = x - 1; y = y - 2; arr[x][y] = 0;}
+                if (x - 1 >= 0  && y - 2 >= 0  && arr[x - 1][y - 2] == 0) { x = x - 1; y = y - 2; arr[x][y] = ++move;}
                 break;
 
             case 8:
-                if (x - 1 >= 0  && y + 2 < len && arr[x - 1][y + 2] == 0) { x = x - 1; y = y + 2; arr[x][y] = 0;}
+                if (x - 1 >= 0  && y + 2 < len && arr[x - 1][y + 2] == 0) { x = x - 1; y = y + 2; arr[x][y] = ++move;}
                 break;
             }
                
-        // System.out.printf("x=%d, y=%d, move=%d ", x, y, move);
+        System.out.printf("x=%d, y=%d, move=%d, variant=%d", x, y, move, variant);
         System.out.println();
         journalMove.put(move, String.format("%d,%d,%d", x, y, variant));
-        return journalMove;
-        
+
+        for (int[] is : arr) {
+            System.out.print("[");
+            for (int i : is) {
+                System.out.printf("%3d ", i);               
+            }
+            System.out.println("]");
+        }
+
+        return journalMove;    
     }
-
-
-
-
-    // private static void horse_move(int x, int y, int[][] arr, int move) {
-    //     if(move == 25) return;
-
-    //     int len = arr.length;
-
-    //     arr[x][y] = ++move;
-    //     if        (x + 2 < len && y + 1 < len && arr[x + 2][y + 1] == 0) { horse_move(x + 2, y + 1, arr, move); arr[x][y] = 0;
-    //     } else if (x + 2 < len && y - 1 >= 0  && arr[x + 2][y - 1] == 0) { horse_move(x + 2, y - 1, arr, move); arr[x][y] = 0;
-    //     } else if (x + 1 < len && y + 2 < len && arr[x + 1][y + 2] == 0) { horse_move(x + 1, y + 2, arr, move); arr[x][y] = 0;
-    //     } else if (x + 1 < len && y - 2 >= 0  && arr[x + 1][y - 2] == 0) { horse_move(x + 1, y - 2, arr, move); arr[x][y] = 0;
-    //     } else if (x - 2 >= 0  && y - 1 >= 0  && arr[x - 2][y - 1] == 0) { horse_move(x - 2, y - 1, arr, move); arr[x][y] = 0;
-    //     } else if (x - 2 >= 0  && y + 1 < len && arr[x - 2][y + 1] == 0) { horse_move(x - 2, y + 1, arr, move); arr[x][y] = 0;
-    //     } else if (x - 1 >= 0  && y - 2 >= 0  && arr[x - 1][y - 2] == 0) { horse_move(x - 1, y - 2, arr, move); arr[x][y] = 0;
-    //     } else if (x - 1 >= 0  && y + 2 < len && arr[x - 1][y + 2] == 0) { horse_move(x - 1, y + 2, arr, move); arr[x][y] = 0;
-    //     }
-
-    // }
 }
