96d555ef434132faba958c32a0ae01e06a93e285
Dmitry <dimakameya@gmail.com>
2023-05-23 23:03:58 +0500

    DZ6

--- DZ/DZ6/Task1_magazin_tehniki.java
+++ DZ/DZ6/Task1_magazin_tehniki.java
@@ -1,5 +1,4 @@
 package JavaProject.DZ.DZ6;
-
 import java.util.*;
 
 /*Подумать над структурой класса Ноутбук для магазина техники — выделить поля и методы. Реализовать в Java.
@@ -17,9 +16,9 @@ import java.util.*;
 */
 public class Task1_magazin_tehniki {
     public static void main(String[] args) {
-        Laptop laptop1 = new Laptop("Samsung", 16, 512, "Windows", "cherry");
-        Laptop laptop2 = new Laptop("Lenovo", 8, 256, "Windows", "black");
-        Laptop laptop3 = new Laptop("Macbook", 32, 512, "MacOS", "silver");
+        Laptop laptop1 = new Laptop("Samsung", 16, 512, "windows", "cherry");
+        Laptop laptop2 = new Laptop("Lenovo", 8, 256, "windows", "black");
+        Laptop laptop3 = new Laptop("Macbook", 32, 512, "macos", "silver");
 
         Set<Laptop> laptops = new HashSet<>();
         laptops.add(laptop1);
@@ -27,17 +26,12 @@ public class Task1_magazin_tehniki {
         laptops.add(laptop3);
 
         System.out.println(laptops);
-//        System.out.println(laptop2);
-//        System.out.println(laptop3);
 
         Map<Integer, String> filter = new HashMap<>();
-//        filter.put(1, "ОЗУ");
-//        filter.put(2, "Объем жесткого диска");
-//        filter.put(3, "Операционная система");
-//        filter.put(4, "Цвет");
 
         Scanner scanner = new Scanner(System.in);
         boolean flag = true;
+
         while (flag) {
             System.out.println("Задайте критерии фильтрации: \n" +
                     "1 - ОЗУ\n" +
@@ -45,31 +39,72 @@ public class Task1_magazin_tehniki {
                     "3 - Операционная система\n" +
                     "4 - Цвет\n" +
                     "5 - Запустить поиск");
-            int input = scanner.nextInt();
-            switch (input) {
-                case 1:
-                    System.out.println("Введите минимальный объем памяти: ");
-                    filter.put(1, scanner.next());
-                    break;
-                case 2:
-                    System.out.println("Введите минимальный объем жесткого диска:");
-                    filter.put(2, scanner.next());
-                    break;
-                case 3:
-                    System.out.println("Введите операционную систему: ");
-                    filter.put(3, scanner.next());
-                    break;
-                case 4:
-                    System.out.println("Введите цвет ноутбука:");
-                    filter.put(4, scanner.next());
-                    break;
-                case 5:
-                    flag = false;
-                    break;
+
+            if (scanner.hasNextInt()) {
+                int input = scanner.nextInt();
+                switch (input) {
+                    case 1:
+                        System.out.println("Введите минимальный объем памяти: ");
+                        filter.put(1, scanner.next());
+                        break;
+                    case 2:
+                        System.out.println("Введите минимальный объем жесткого диска:");
+                        filter.put(2, scanner.next());
+                        break;
+                    case 3:
+                        System.out.println("Введите операционную систему: ");
+                        filter.put(3, scanner.next().toLowerCase());
+                        break;
+                    case 4:
+                        System.out.println("Введите цвет ноутбука:");
+                        filter.put(4, scanner.next().toLowerCase());
+                        break;
+                    case 5:
+                        flag = false;
+                        break;
+                }
+            } else {
+                flag = false;
+                System.out.println("Вы ввели не число");
+            }
+            System.out.println(filter);
+
+        }
+        Set<Laptop> results = new HashSet<>();
+        for (Laptop element : laptops) {
+            if (filter.containsKey(1)) {
+                if (Integer.parseInt(filter.get(1)) <= element.getRam()) {
+                    results.add(element);
+                } else
+                    continue;
+            }
+
+            if (filter.containsKey(2)) {
+                if (Integer.parseInt(filter.get(2)) <= element.getHdd()) {
+                    results.add(element);
+                } else {
+                    results.remove(element);
+                    continue;
+                }
+            }
+            if (filter.containsKey(3)) {
+                if (filter.get(3).equals(element.getOs())) {
+                    results.add(element);
+                } else {
+                    results.remove(element);
+                    continue;
+                }
+            }
+            if (filter.containsKey(4)) {
+                if (filter.get(4).equals(element.getColor())) {
+                    results.add(element);
+                } else {
+                    results.remove(element);
+                }
             }
         }
-
-        System.out.println(filter);
-
+        for (Laptop laptop : results) {
+            System.out.println(laptop);
+        }
     }
 }
